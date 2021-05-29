11eb586feea32628d364047bccb0c33e66d8f3f9
Dylan <dcren2010@gmail.com>
2021-05-29 09:01:00 -0400

    build native ws and ws server

--- justWsServer.js
+++ justWsServer.js
@@ -6,5 +6,12 @@ const server = http.createServer((req, res) => {
 });
 
 const wss = new websocket.Server({server})
+wss.on('headers', (headers, req) => {
+	console.log(headers);
+})
 
+wss.on('connection', (ws, req) => {
+	ws.send('Welcome to the websocket server!!!')
+	ws.on('message', msg => console.log(msg))
+})
 server.listen(4000);
